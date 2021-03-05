e5153d2ecb52e7d951a52fc3a734e297c5139721
Aman Dhurwey <2017030@iiitdmj.ac.in>
2021-03-05 09:02:09 +0530

    Add debug code

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,9 @@
 exports.handler = async function () {
   const { NAME } = process.env
+
+  // debugging
+  console.log(process.env.NAME)
+  console.log(process.env.CONTEXT)
   return {
     statusCode: 200,
     body: `Hello ${NAME}`,
